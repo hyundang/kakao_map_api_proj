e33843afcddcbf8e692d0eca2b0bf8910c3aab11
hyundang <hyunjin5697@yonsei.ac.kr>
2022-01-25 16:39:35 +0900

    chore: path alias setting

--- config-overrides.js
+++ config-overrides.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 module.exports = override(
   addWebpackAlias({
-    '@components/*': path.resolve(__dirname, 'src/components/*'),
-    '@pages/*': path.resolve(__dirname, 'src/pages/*'),
-    '@apis/*': path.resolve(__dirname, 'src/apis/*'),
-    '@libs/*': path.resolve(__dirname, 'src/libs/*'),
-    '@modules/*': path.resolve(__dirname, 'src/modules/*'),
-    '@interfaces/*': path.resolve(__dirname, 'src/interfaces/*'),
+    '@components': path.resolve(__dirname, 'src/components'),
+    '@pages': path.resolve(__dirname, 'src/pages'),
+    '@apis': path.resolve(__dirname, 'src/apis'),
+    '@libs': path.resolve(__dirname, 'src/libs'),
+    '@modules': path.resolve(__dirname, 'src/modules'),
+    '@interfaces': path.resolve(__dirname, 'src/interfaces'),
   }),
 );
