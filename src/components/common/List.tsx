3b94debcd5d5cde3784bda75603508919cf5ed6b
hyundang <hyunjin5697@yonsei.ac.kr>
2022-01-30 01:35:26 +0900

    fix: List component

--- src/components/common/List.tsx
+++ src/components/common/List.tsx
@@ -1,6 +1,6 @@
 import React, { HTMLAttributes } from "react";
 import styled, { css } from "styled-components";
-import { UserDataProps } from "@interfaces/User";
+import { UserDataProps } from "@interfaces/index";
 
 interface ListItemPrps extends HTMLAttributes<HTMLElement> {
   userData: UserDataProps;
@@ -24,11 +24,17 @@ const ListItem = ({
       onClick={onClickUser}
     >
       <div>{userData.address}</div>
-      <div>{userData.company}</div>
+      <div>{userData.alias}</div>
       <div>{userData.addressDetail}</div>
       <div>{userData.caution}</div>
-      <button className="del_btn" onClick={onClickBtn}>
-        {isTitle ? "추가" : "삭제"}
+      <button
+        className="del_btn"
+        onClick={(e) => {
+          e.stopPropagation();
+          onClickBtn();
+        }}
+      >
+        {isTitle ? <b>추가</b> : "삭제"}
       </button>
     </ItemWrap>
   );
@@ -37,7 +43,7 @@ const ListItem = ({
 interface ListProps extends HTMLAttributes<HTMLElement> {
   userDataList: UserDataProps[];
   onClickCreateBtn: () => void;
-  onClickDelBtn: () => void;
+  onClickDelBtn: (userId: number) => void;
   onClickUser: (data: UserDataProps) => void;
 }
 const List = ({
@@ -51,7 +57,7 @@ const List = ({
   const title: UserDataProps = {
     id: -1,
     address: "주소",
-    company: "회사",
+    alias: "주소별칭",
     addressDetail: "상세주소",
     caution: "유의사항",
   };
@@ -59,19 +65,21 @@ const List = ({
   return (
     <Container id={id} className={className}>
       <ListItem isTitle userData={title} onClickBtn={onClickCreateBtn} />
-      {userDataList.length === 0 ? (
-        <div className="no_data">데이터가 존재하지 않습니다</div>
-      ) : (
-        userDataList.map((user, idx) => (
-          <ListItem
-            key={idx}
-            id={`${idx}`}
-            userData={user}
-            onClickBtn={onClickDelBtn}
-            onClickUser={() => onClickUser(user)}
-          />
-        ))
-      )}
+      <div className="list_item_wrap">
+        {userDataList.length === 0 ? (
+          <div className="no_data">데이터가 존재하지 않습니다</div>
+        ) : (
+          userDataList.map((user) => (
+            <ListItem
+              key={user.id}
+              id={`${user.id}`}
+              userData={user}
+              onClickBtn={() => onClickDelBtn(user.id)}
+              onClickUser={() => onClickUser(user)}
+            />
+          ))
+        )}
+      </div>
     </Container>
   );
 };
@@ -88,9 +96,15 @@ const Container = styled.ul`
   .no_data {
     width: 100%;
     height: 40px;
-    margin: 20px;
+    padding-top: 20px;
     text-align: center;
     font-size: 20px;
+  }
+
+  .list_item_wrap {
+    width: 100%;
+    height: 600px;
+    overflow-y: scroll;
   }
 `;
 
@@ -99,28 +113,37 @@ interface ItemWrapProps {
 }
 const ItemWrap = styled.div<ItemWrapProps>`
   width: 100%;
-  height: 40px;
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr 100px;
+  grid-template-columns: 1fr 1fr 1fr 1fr 70px;
+  ${({ isTitle }) =>
+    isTitle &&
+    css`
+      overflow-y: scroll;
+      height: 42px;
+    `}
 
   > div {
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    word-wrap: normal;
-    overflow: hidden;
+    padding: 0 5px;
     border: 1px solid black;
-    line-height: 40px;
     ${({ isTitle }) =>
       isTitle
         ? css`
+            box-sizing: border-box;
+            height: 40px;
+            line-height: 40px;
             font-weight: 700;
             text-align: center;
-            font-size: 20px;
+            font-size: 16px;
           `
         : css`
+            height: 55px;
+            line-height: 40px;
+            white-space: nowrap;
+            overflow-x: scroll;
+            overflow-y: hidden;
             font-weight: 400;
             text-align: start;
-            font-size: 16px;
+            font-size: 14px;
           `};
   }
 `;
