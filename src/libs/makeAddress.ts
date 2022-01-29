0991dc82fee5bd0442194b14edba562b53b66ec2
hyundang <hyunjin5697@yonsei.ac.kr>
2022-01-30 01:40:35 +0900

    feat: MapModule

---
+++ src/libs/makeAddress.ts
@@ -0,0 +1,79 @@
+const makeAddress = (item: any) => {
+  if (!item) {
+    return;
+  }
+
+  var name = item.name,
+    region = item.region,
+    land = item.land,
+    isRoadAddress = name === "roadaddr";
+
+  var sido = "",
+    sigugun = "",
+    dongmyun = "",
+    ri = "",
+    rest = "";
+
+  if (hasArea(region.area1)) {
+    sido = region.area1.name;
+  }
+
+  if (hasArea(region.area2)) {
+    sigugun = region.area2.name;
+  }
+
+  if (hasArea(region.area3)) {
+    dongmyun = region.area3.name;
+  }
+
+  if (hasArea(region.area4)) {
+    ri = region.area4.name;
+  }
+
+  if (land) {
+    if (hasData(land.number1)) {
+      if (hasData(land.type) && land.type === "2") {
+        rest += "산";
+      }
+
+      rest += land.number1;
+
+      if (hasData(land.number2)) {
+        rest += "-" + land.number2;
+      }
+    }
+
+    if (isRoadAddress === true) {
+      if (checkLastString(dongmyun, "면")) {
+        ri = land.name;
+      } else {
+        dongmyun = land.name;
+        ri = "";
+      }
+
+      if (hasAddition(land.addition0)) {
+        rest += " " + land.addition0.value;
+      }
+    }
+  }
+
+  return [sido, sigugun, dongmyun, ri, rest].join(" ");
+};
+
+const hasArea = (area: any) => {
+  return !!(area && area.name && area.name !== "");
+};
+
+const hasData = (data: any) => {
+  return !!(data && data !== "");
+};
+
+const checkLastString = (word: any, lastString: any) => {
+  return new RegExp(lastString + "$").test(word);
+};
+
+const hasAddition = (addition: any) => {
+  return !!(addition && addition.value);
+};
+
+export default makeAddress;
